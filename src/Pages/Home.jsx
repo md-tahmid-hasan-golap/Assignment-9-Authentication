c8abface5b101e702fcdcba8e326eb5b53a3c829
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-07-13 02:19:39 +0600

    add a all json files

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,16 @@ import Banner from "../Components/Banner";
 const Home = () => {
   return (
     <div>
+      {/* 1. Slider Section: */}
       <Banner></Banner>
+
+      {/* 2. Trending Apps: */}
+
+      {/* Productivity apps */}
+
+      {/* Education apps */}
+
+      {/* Health apps */}
     </div>
   );
 };
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
+        index: true,
         Component: Home,
       },
     ],
